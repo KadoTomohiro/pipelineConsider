8aa1e55387bc979583b8c9905fd9715023e031eb
KadoTomohiro <corner0427@gmail.com>
2023-10-30 01:49:32 +0900

    sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {from, map, Observable} from 'rxjs';
+import {Outlet, Pipeline} from "./pipeline/pipeline";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipelineConsider';
+
+  constructor() {
+    const pipeline: Pipeline<string, {initial$: string} > = new Pipeline<string, {initial$: string}>(null, undefined, {
+      initial$: [
+        map((s: string | null) => s + 'a'),
+      ],
+    })
+
+    pipeline.outlets.default$
+
+    const o: Outlet = {
+        initial$: from(['a', 'b', 'c']),
+    }
+  }
 }
