ce39fc5db567e7d77e2faa418d9712c73a5f9b4a
KadoTomohiro <corner0427@gmail.com>
2023-11-08 01:29:52 +0900

    add pulse line

--- src/app/pulse-line/inlet.ts
+++ src/app/pulse-line/inlet.ts
@@ -1,2 +1,18 @@
-export class Inlet {
+import {Tank} from "./tank";
+import {Observable, Observer, Subject, Subscription} from "rxjs";
+
+/**
+ * タンクへのデータ投入口
+ */
+export class Inlet<T> {
+  private subscription: Subscription;
+  constructor(source: Observable<T>, private tankObserver: Observer<T>, handlerOption: Partial<Observer<T>> = {}) {
+    const inletObserver = Object.assign({}, tankObserver, handlerOption)
+    this.subscription = source.subscribe(inletObserver);
+  }
+
+  reject() {
+    this.subscription.unsubscribe();
+  }
+
 }
--- src/app/pulse-line/line.ts
+++ src/app/pulse-line/line.ts
@@ -4,6 +4,10 @@ import {Tank} from "./tank";
 export class Line<T, U> {
   constructor(private source: Observable<T>, private transformer: OperatorFunction<T, U>) {}
 
+  /**
+   * Connects the line to a tank.
+   * @param tank The tank to connect to.
+   */
   connect(tank: Tank<U>): void {
     this.source.pipe(this.transformer).subscribe((value) => {
       tank.input(value);
